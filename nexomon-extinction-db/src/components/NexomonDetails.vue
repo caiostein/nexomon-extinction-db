a78c777c3e9673cbe2ad3e11cc026c244931aa2d
caiostein <caio.kaki98@gmail.com>
2024-10-05 23:33:27 -0300

    Frontend setup

---
+++ nexomon-extinction-db/src/components/NexomonDetails.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+      <h1>Nexomon Database</h1>
+      <ul>
+        <li v-for="nexomon in nexomons" :key="nexomon.Name">
+          {{ nexomon.Name }}
+          <img :src="nexomon.Sprites.Regular" alt="Sprite" />
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  import data from '../../../Assets/nexomon_extinction_database.json';
+  
+  export default {
+    data() {
+      return {
+        nexomons: data // Load your JSON data here
+      };
+    }
+  };
+  </script>
+  
